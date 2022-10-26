d108ab6b0132c5836b20dca53804403c73d2d7e7
Byeunghyeun Kim <byeukim@c8r1s1.42seoul.kr>
2022-10-26 19:28:40 +0900

    a

--- Test/requirements/mariadb/tools/conf.sh
+++ Test/requirements/mariadb/tools/conf.sh
@@ -1,9 +1,9 @@
 sed -i "s/.*bind-address\s.*/bind-address=0.0.0.0\nport=3306/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mysql -e "CREATE DATABASE ${MARIADB_DB};"
-mysql -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PWD}';"
-mysql -e "GRANT ALL ON ${MARIADB_DB}.* TO '${MARIADB_USER}'@'%';"
-mysql -e "ALTER USER '${MARIADB_ADMIN}'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PWD}'";
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE DATABASE ${MARIADB_DB};"
+mysql -u root -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PWD}';"
+mysql -u root -e "GRANT ALL ON ${MARIADB_DB}.* TO '${MARIADB_USER}'@'%';"
+mysql -u root -e "ALTER USER '${MARIADB_ADMIN}'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PWD}'";
+mysql -u root -e "FLUSH PRIVILEGES;"
 
 exec mysqld
