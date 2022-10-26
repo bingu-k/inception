139de7a169cdf97f3336449c74877db40d8002cc
Byeunghyeun Kim <byeukim@c8r1s1.42seoul.kr>
2022-10-26 20:58:30 +0900

    q

--- Test/requirements/mariadb/tools/conf.sh
+++ Test/requirements/mariadb/tools/conf.sh
@@ -1,9 +1,13 @@
 sed -i "s/.*bind-address\s.*/bind-address=0.0.0.0\nport=3306/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mysql -u root -e "CREATE DATABASE ${MARIADB_DB};"
-mysql -u root -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PWD}';"
-mysql -u root -e "GRANT ALL ON ${MARIADB_DB}.* TO '${MARIADB_USER}'@'%';"
-mysql -u root -e "ALTER USER '${MARIADB_ADMIN}'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PWD}'";
-mysql -u root -e "FLUSH PRIVILEGES;"
+service mysql start;
+
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DB};"
+mysql -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PWD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MARIADB_DB}.* TO '${MARIADB_USER}'@'%';"
+mysql -e "ALTER USER '${MARIADB_ADMIN}'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PWD}'";
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u $MARIADB_ADMIN -p $MARIADB_ADMIN_PWD shutdown
 
 exec mysqld
