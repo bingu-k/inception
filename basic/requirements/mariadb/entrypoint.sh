4c6868c4d733112d9063c1890ed9bbf3c0c0ec78
Byeunghyeun Kim <byeukim@c5r5s1.42seoul.kr>
2022-11-24 11:49:46 +0900

    Pre-success mariadb

--- basic/requirements/mariadb/entrypoint.sh
+++ basic/requirements/mariadb/entrypoint.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-chown -R mysql:mysql /var/lib/mysql
-echo "Fuck : 2"
+chown -R 777 /var/lib/mysql/
+
+chown -R mysql:mysql /var/lib/mysql/
+
 FILE="/var/lib/mysql/$MYSQL_DATABASE"
 
 if [ ! -f "$FILE" ]; then
-	echo "Fuck : in1"
-	service mysql restart
-	sleep 1
-	echo "Fuck : in2"
-    
-	mysql -u ${MYSQL_ROOT} -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
-    mysql -u ${MYSQL_ROOT} -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
-    mysql -u ${MYSQL_ROOT} -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'"
-    mysql -u ${MYSQL_ROOT} -e "FLUSH PRIVILEGES"
-    mysql -u ${MYSQL_ROOT} -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
-    mysql -u ${MYSQL_ROOT} -e "FLUSH PRIVILEGES"
-    mysql -u ${MYSQL_ROOT} -e $MYSQL_DATABASE -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD
-    
-	mysqladmin -h localhost -p $MYSQL_ROOT_PASSWORD shutdown
+    service mysql start
+
+    mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;";
+    mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';";
+    mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';";
+    mysql -e "FLUSH PRIVILEGES;";
+
+    mysql -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';";
+    mysql -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;";
+
+    mysql -hlocalhost $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD;
+    mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown;
 fi
 
-echo "Fuck : 3"
-exec service mysql start | myslqd
+exec mysqld_safe
