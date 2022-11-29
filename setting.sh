0d507d9e8a4604923e58ca04d6cfb7cbdea6f08b
Byeunghyeun Kim <byeukim@c5r5s1.42seoul.kr>
2022-11-29 13:21:35 +0900

    Finish

--- setting.sh
+++ setting.sh
@@ -14,10 +14,15 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get install -y ca-certificates
 
 # 다양한 통신 프로토콜을 지원하는 데이터 송,수신 패키지
-sudo apt-get install -y curl
+sudo apt-get install -y curl make
 
 # Repository를 추가 및 삭제할 수 있도록 설정하는 패키지
 sudo apt-get install -y software-properties-common
 
-# sudo 권한 부여
-# vim /etc/sudoers에서 유저이름 추가
+# openssl s_client -connect localhost:443 -tls1_3 //TLS1.3 확인
+# openssl s_client -connect localhost:443 -tls1_2 //TLS1.2 확인
+# VM 에서 확인하기!
+
+# 크롬으로 http 접근 가능한지 확인위한 설치
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
